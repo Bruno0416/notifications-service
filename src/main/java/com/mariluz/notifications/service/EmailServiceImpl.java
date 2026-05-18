0a7e4da9d18d9bb78dbc65982ad4133fdb61b642
Bruno0416 <brunomcalderonv@gmail.com>
2026-05-18 17:12:20 -0400

    comentario sobre notification service

--- src/main/java/com/mariluz/notifications/service/EmailServiceImpl.java
+++ src/main/java/com/mariluz/notifications/service/EmailServiceImpl.java
@@ -1,3 +1,10 @@
+/*
+    NOTA: Ya que el servicio de resend no tiene
+    configurado un dominio (correo oficial de empresa/tienda)
+    el correo unicamente puede ser enviado al duenio de la api(brunomcalderonv@gmail.com)
+    -> por lo que cualquier intento de enviar correo a otra direccion va a  dar error
+*/
+
 package com.mariluz.notifications.service;
 
 import com.mariluz.notifications.model.EmailTemplate;
