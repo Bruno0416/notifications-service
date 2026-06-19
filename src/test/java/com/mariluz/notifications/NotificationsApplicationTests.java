12afa26e349450b6570e339cac8ca222364b23b7
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-19 16:49:49 -0400

    Update NotificationsApplicationTests.java

--- src/test/java/com/mariluz/notifications/NotificationsApplicationTests.java
+++ src/test/java/com/mariluz/notifications/NotificationsApplicationTests.java
@@ -3,9 +3,12 @@ package com.mariluz.notifications;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "RESEND_API_KEY=re_123456789987654321" 
+})
 class NotificationsApplicationTests {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
